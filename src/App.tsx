117e9233ea0189174083d61c5fd2f335538f0f82
joaoAugustoParaty <joaodrow@gmail.com>
2022-02-11 16:15:56 -0300

    reducers1

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,37 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useReducer } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+type reducerState ={
+  count: number;
 }
 
-export default App
+type reducerAction = {
+  type: string;
+
+}
+const initialState = {count: 0};
+const reducer = (state: reducerState, action: reducerAction ) => {
+
+  switch(action.type){
+    case 'ADD':
+      return {...state, count: state.count + 1};
+      break;
+      case 'DEL':
+        return {...state, count: state.count -1};
+        break;
+        case 'RESET':
+          return initialState;
+          break;
+  }
+
+  return state;
+
+}
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+   <div className="p-5">
+    
+   </div>
+  );
+}
