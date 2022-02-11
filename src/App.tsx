80ef75a4624072a5fc70f4ea12b178cbcf5dad1c
joaoAugustoParaty <joaodrow@gmail.com>
2022-02-11 17:50:31 -0300

    reducersconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,17 @@
-import { useReducer } from "react";
+import {useContagem} from './reducers/contagem';
 
-type reducerState ={
-  count: number;
-}
 
-type reducerAction = {
-  type: string;
-
-}
-const initialState = {count: 0};
-const reducer = (state: reducerState, action: reducerAction ) => {
-
-  switch(action.type){
-    case 'ADD':
-      return {...state, count: state.count + 1};
-      break;
-      case 'DEL':
-        if(state.count > 0){
-        return {...state, count: state.count -1};
-        }
-        break;
-        case 'RESET':
-          return initialState;
-          break;
-  }
-
-  return state;
-
-}
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [contagem, contagemdispatch] = useContagem();
+ 
 
   return (
    <div className="p-5">
-    Contagem: {state.count}
+    Contagem: {contagem.count}
     <hr/>
-    <button className="p-3" onClick={()=>dispatch({type:'ADD'})}>Adicionar</button>
-    <button className="p-3" onClick={()=>dispatch({type:'DEL'})}>Remover</button>
-    <button className="p-3" onClick={()=>dispatch({type:'RESET'})}>Resetar</button>
+    <button className="p-3" onClick={()=>contagemdispatch({type:'ADD'})}>Adicionar</button>
+    <button className="p-3" onClick={()=>contagemdispatch({type:'DEL'})}>Remover</button>
+    <button className="p-3" onClick={()=>contagemdispatch({type:'RESET'})}>Resetar</button>
    </div>
   );
 }
