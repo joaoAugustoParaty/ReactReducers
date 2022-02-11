a92d790285d2d0262584385188e0058dda621ef5
joaoAugustoParaty <joaodrow@gmail.com>
2022-02-11 19:08:51 -0300

    usereducer1

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import {useContagem} from './reducers/contagem';
-
-
 const App = () => {
-  const [contagem, contagemdispatch] = useContagem();
- 
 
   return (
-   <div className="p-5">
-    Contagem: {contagem.count}
-    <hr/>
-    <button className="p-3" onClick={()=>contagemdispatch({type:'ADD'})}>Adicionar</button>
-    <button className="p-3" onClick={()=>contagemdispatch({type:'DEL'})}>Remover</button>
-    <button className="p-3" onClick={()=>contagemdispatch({type:'RESET'})}>Resetar</button>
-   </div>
-  );
+    <div className="p-5">
+
+    </div>
+  )
 }
 
 export default App;
