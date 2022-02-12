2df11327b5b54cbef1029a33b106155a7687faf3
joaoAugustoParaty <joaodrow@gmail.com>
2022-02-11 21:19:33 -0300

    usereducer3

--- src/App.tsx
+++ src/App.tsx
@@ -21,15 +21,37 @@ const App = () => {
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
    setNameInput(e.target.value);
   }
+  const deletePerson = (id: string) => {
+    dispatch({
+      type: 'DEL',
+      payload: {id}
+    })
+  }
+
+  const handleOrderButton = () => {
+    dispatch({
+      type: 'ORDER'
+
+    })
+  }
   return (
     <div className="p-5">
       <input className='border-2' type="text" value={nameInput} onChange={handleInputChange}/>
       <button onClick={handleAddButton}>Adicionar</button>
+
       <hr/>
+
+      <button onClick={handleOrderButton}>Ordenar</button>
+      <br/>
       Lista de pessoas:
+      <ul>
      {list.map((item, index) => (
-       <li key ={index}>{item.name}</li>
+       <li key ={index}>
+         {item.name}
+         <button onClick={() => deletePerson(item.id)}>[deletar]</button>
+         </li>
      ))}
+     </ul>
     </div>
   )
 }
